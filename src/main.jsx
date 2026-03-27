7891f2f0a86ab218f80f997cd9eba149c5f5c867
juandavidperez <juandavidperezvergara@gmail.com>
2026-03-27 14:33:05 -0500

    chore: update infrastructure and assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+import { ThemeProvider } from './context/ThemeContext.jsx'
 import { ErrorBoundary } from './components/ErrorBoundary.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </ErrorBoundary>
   </StrictMode>,
 )
